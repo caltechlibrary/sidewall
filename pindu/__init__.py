c2e3d15d6bb03016371982b9e25e65c868b6db85
Michael Hucka <mhucka@caltech.edu>
2019-01-30 14:25:05 -0800

    Initial versions of basic files.

---
+++ pindu/__init__.py
@@ -0,0 +1,19 @@
+'''
+pindu: a library for programming with the Dimensions API
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from .__version__ import __version__, __title__, __description__, __url__
+from .__version__ import __author__, __email__
+from .__version__ import __license__, __copyright__
---
+++ pindu/__version__.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   pindu version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/pindu
+# =============================================================================
+
+__version__     = '0.0.0'
+__title__       = 'pindu'
+__name__        = 'pindu'
+__description__ = '''Python library for interacting with the Dimensions API.'''
+__url__         = 'https://github.com/caltechlibrary/pindu'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>, Betsy Coles <betsycoles@gmail.com>'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause license -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2019 by the California Institute of Technology'
