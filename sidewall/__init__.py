16e0bee956a11ca0ba4ebaafb578802b6d715dbe
Michael Hucka <mhucka@caltech.edu>
2019-02-13 15:47:04 -0800

    Expose more classes

--- sidewall/__init__.py
+++ sidewall/__init__.py
@@ -14,6 +14,14 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-from .__version__ import __version__, __title__, __description__, __url__
-from .__version__ import __author__, __email__
-from .__version__ import __license__, __copyright__
+from .__version__  import __version__, __title__, __description__, __url__
+from .__version__  import __author__, __email__
+from .__version__  import __license__, __copyright__
+
+from .exceptions   import *
+from .author       import Author
+from .dimensions   import Dimensions
+from .organization import Organization
+from .person       import Person
+from .publication  import Publication
+from .researcher   import Researcher
