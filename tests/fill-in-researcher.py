e5c75f73b00b4739db094bf4c7e91cec46e4ba93
Michael Hucka <mhucka@caltech.edu>
2019-02-15 09:57:04 -0800

    1) Prepend parent path instead of appending; 2) call login()

--- tests/fill-in-researcher.py
+++ tests/fill-in-researcher.py
@@ -7,17 +7,19 @@ import sys
 # Allow this program to be executed directly from the 'tests' directory.
 try:
     thisdir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(thisdir, '..'))
+    sys.path.insert(0, os.path.join(thisdir, '..'))
 except:
-    sys.path.append('..')
-
+    sys.path.insert(0, '..')
 
 import sidewall
-from sidewall import Researcher
+from sidewall import dimensions, Researcher
+sidewall.set_debug(True)
+
+# Log in so that the Researcher object can fill in 'orcid' value when needed.
+dimensions.login()
 
 with open('test-data/example-incomplete-researcher.json', 'r') as f:
     data = jsonlib.load(f)
-
 researchers = []
 for item in data['researchers']:
     researchers.append(Researcher(item))
