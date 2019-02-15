dbe85f2b0260e8673c0556743ad0930297742f4e
Michael Hucka <mhucka@caltech.edu>
2019-02-14 20:01:49 -0800

    Import more symbols

--- sidewall/__init__.py
+++ sidewall/__init__.py
@@ -14,15 +14,15 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-from .__version__  import __version__, __title__, __description__, __url__
-from .__version__  import __author__, __email__
-from .__version__  import __license__, __copyright__
+from .__version__    import __version__, __title__, __description__, __url__
+from .__version__    import __author__, __email__
+from .__version__    import __license__, __copyright__
 
-from .exceptions   import *
-from .author       import Author
-from .dimensions   import Dimensions
-from .organization import Organization
-from .person       import Person
-from .publication  import Publication
-from .researcher   import Researcher
-from .debug        import set_debug
+from .exceptions     import *
+from .author         import Author
+from .debug          import set_debug
+from .dimensions_cls import dimensions
+from .organization   import Organization
+from .person         import Person
+from .publication    import Publication
+from .researcher     import Researcher
