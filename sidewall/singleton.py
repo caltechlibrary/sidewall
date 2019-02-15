c992f99c4d6699e11118126152fcc3049bdd00c9
Michael Hucka <mhucka@caltech.edu>
2019-02-14 20:01:16 -0800

    Add Singleton class definition

---
+++ sidewall/singleton.py
@@ -0,0 +1,30 @@
+'''
+singleton.py: singleton class as defined by Guido Van Rossum
+
+Original code: https://www.python.org/download/releases/2.2.3/descrintro/
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+class Singleton(object):
+
+    def __new__(cls, *args, **kwds):
+        it = cls.__dict__.get("__it__")
+        if it is not None:
+            return it
+        cls.__it__ = it = object.__new__(cls)
+        it.init(*args, **kwds)
+        return it
+
+    def init(self, *args, **kwds):
+        pass
