9449f3ea65387a5456f3a234f0919894d42645d9
Michael Hucka <mhucka@caltech.edu>
2019-02-15 20:05:35 -0800

    Test Publication object

---
+++ tests/fill-in-publications.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json as jsonlib
+import os
+import sys
+
+# Allow this program to be executed directly from the 'tests' directory.
+try:
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(thisdir, '..'))
+except:
+    sys.path.insert(0, '..')
+
+import sidewall
+from sidewall import dimensions, Publication
+sidewall.set_debug(True)
+
+# Log in so that the Researcher object can fill in fields when needed.
+dimensions.login()
+
+with open('test-data/example-two-publications.json', 'r') as f:
+    data = jsonlib.load(f)
+publications = []
+for item in data['publications']:
+    publications.append(Publication(item))
+
+for pub in publications:
+    print('id {} title {}'.format(pub.id, pub.title))
+
+import pdb; pdb.set_trace()
