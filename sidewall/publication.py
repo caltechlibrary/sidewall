217b8d0944c2e3d83a43961ab5e1072320a86d3c
Michael Hucka <mhucka@caltech.edu>
2019-02-15 16:34:24 -0800

    Finish implementing Publication

--- sidewall/publication.py
+++ sidewall/publication.py
@@ -14,69 +14,74 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
+from .author import Author
 from .core import DimensionsCore
+from .debug import log
 from .exceptions import *
+from .journal import Journal
 
 class Publication(DimensionsCore):
+    _attributes = ['altmetric', 'author_affiliations', 'book_doi',
+                   'book_series_title', 'book_title', 'date', 'date_inserted',
+                   'doi', 'field_citation_ratio', 'id', 'issn', 'issue',
+                   'journal', 'linkout', 'mesh_terms', 'open_access', 'pages',
+                   'pmcid', 'pmid', 'proceedings_title', 'publisher',
+                   'references', 'relative_citation_ratio',
+                   'research_org_country_names', 'research_org_state_names',
+                   'supporting_grant_ids' 'times_cited', 'title', 'type',
+                   'volume', 'year'] + DimensionsCore._attributes
 
-    def _update_attributes(self, pub):
+
+    def _update_attributes(self, data):
+        if __debug__: log('updating object {} using {}', id(self), data)
+        if not isinstance(data, dict):
+            raise InternalError('Data not in dict format')
         super()._update_attributes(data)
 
         # https://docs.dimensions.ai/dsl/data.html#data
-        self.altmetric               = pub.get('altmetric', '')
-        self.book_doi                = pub.get('book_doi', '')
-        self.book_series_title       = pub.get('book_series_title', '')
-        self.book_title              = pub.get('book_title', '')
-        self.date                    = pub.get('date', '')
-        self.date_inserted           = pub.get('date_inserted', '')
-        self.doi                     = pub.get('doi', '')
-        self.field_citation_ratio    = pub.get('field_citation_ratio', '')
-        self.id                      = pub.get('id', '')
-        self.issn                    = pub.get('issn', '')
-        self.issue                   = pub.get('issue', '')
-        self.linkout                 = pub.get('linkout', '')
-        self.journal                 = pub.get('journal', '')
-        self.open_access             = pub.get('open_access', '')
-        self.pages                   = pub.get('pages', '')
-        self.pmcid                   = pub.get('pmcid', '')
-        self.pmid                    = pub.get('pmid', '')
-        self.proceedings_title       = pub.get('proceedings_title', '')
-        self.publisher               = pub.get('publisher', '')
-        self.references              = pub.get('references', '')
-        self.relative_citation_ratio = pub.get('relative_citation_ratio', '')
-        self.times_cited             = pub.get('times_cited', '')
-        self.title                   = pub.get('title', '')
-        self.type                    = pub.get('type', '')
-        self.volume                  = pub.get('volume', '')
-        self.year                    = pub.get('year', '')
+        self.altmetric                  = data.get('altmetric', '')
+        self.book_doi                   = data.get('book_doi', '')
+        self.book_series_title          = data.get('book_series_title', '')
+        self.book_title                 = data.get('book_title', '')
+        self.date                       = data.get('date', '')
+        self.date_inserted              = data.get('date_inserted', '')
+        self.doi                        = data.get('doi', '')
+        self.field_citation_ratio       = data.get('field_citation_ratio', '')
+        self.id                         = data.get('id', '')
+        self.issn                       = data.get('issn', '')
+        self.issue                      = data.get('issue', '')
+        self.linkout                    = data.get('linkout', '')
+        self.mesh_terms                 = data.get('mesh_terms', '')
+        self.open_access                = data.get('open_access', '')
+        self.pages                      = data.get('pages', '')
+        self.pmcid                      = data.get('pmcid', '')
+        self.pmid                       = data.get('pmid', '')
+        self.proceedings_title          = data.get('proceedings_title', '')
+        self.publisher                  = data.get('publisher', '')
+        self.references                 = data.get('references', '')
+        self.relative_citation_ratio    = data.get('relative_citation_ratio', '')
+        self.research_org_country_names = data.get('research_org_country_names', '')
+        self.research_org_state_names   = data.get('research_org_state_names', '')
+        self.supporting_grant_ids       = data.get('supporting_grant_ids', '')
+        self.times_cited                = data.get('times_cited', '')
+        self.title                      = data.get('title', '')
+        self.type                       = data.get('type', '')
+        self.volume                     = data.get('volume', '')
+        self.year                       = data.get('year', '')
 
+        # Journal is an object.
+        self.journal = Journal(data['journal']) if 'journal' in data else ''
+
+        # Affiliations are a list.
         self.author_affiliations = []
-        if 'author_affiliations' in pub:
+        if 'author_affiliations' in data:
             # All cases I've seen so far have been a list containing 1 list.
-            if len(pub['author_affiliations']) > 1:
+            if len(data['author_affiliations']) > 1:
                 raise DataMismatch('Affiliations list holds more than one list')
 
-            for a in pub['author_affiliations'][0]:
+            for a in data['author_affiliations'][0]:
                 self.author_affiliations.append(Author(a))
 
 
-        # FIXME still to do:
-        #
-        # journal_lists
-        # supporting_grant_ids
-        # recent_citations
-        # FOR
-        # funders
-        # funder_countries
-        # references
-        # research_org_cities
-        # research_org_countries
-        # research_org_country_names
-        # research_org_state_codes
-        # research_org_state_names
-        # research_orgs
-        # researchers
-
-
     def __repr__(self):
-        return "<Publication 0x{0:x}>".format(id(self))
+        return "<Publication {}>".format(self.id)
