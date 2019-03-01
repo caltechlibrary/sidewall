cbdfa7332c51ec58f6af271f09400e7b947985f8
Michael Hucka <mhucka@caltech.edu>
2019-03-01 12:38:25 -0800

    Add another example of searching and printing values

---
+++ examples/print-authors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    pubs-missing-dois.py
+# @brief   Example use of Sidewall to print SBML pubs missing DOI
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/sidewall
+# =============================================================================
+
+import os
+import sys
+
+# Allow this program to be executed directly from the 'examples' directory.
+try:
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(thisdir, '..'))
+except:
+    sys.path.insert(0, '..')
+
+import sidewall
+from sidewall import dimensions
+
+if len(sys.argv) > 1 and sys.argv[1] == '-d':
+    sidewall.set_debug(True)
+
+dimensions.login()
+
+print('sending query to Dimensions')
+(total, pubs) = dimensions.query('search publications in title_only for "SBML" where year=2003 return publications')
+print('got back {} publications'.format(total))
+
+for p in pubs:
+    print('-'*70)
+    print('{}'.format(p.title))
+    for a in p.authors:
+        print('{} {} ({})'.format(a.first_name, a.last_name, a.orcid))
