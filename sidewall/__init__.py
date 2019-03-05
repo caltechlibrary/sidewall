c99a318d3b86467f4ff208bc0d35edf85089e5ae
Michael Hucka <mhucka@caltech.edu>
2019-03-05 12:54:21 -0800

    Adjust for renamed dimensions_cls.py file

--- sidewall/__init__.py
+++ sidewall/__init__.py
@@ -14,17 +14,17 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-from .__version__    import __version__, __title__, __description__, __url__
-from .__version__    import __author__, __email__
-from .__version__    import __license__, __copyright__
+from .__version__  import __version__, __title__, __description__, __url__
+from .__version__  import __author__, __email__
+from .__version__  import __license__, __copyright__
 
-from .exceptions     import *
-from .debug          import set_debug
-from .dimensions_cls import dimensions
+from .exceptions   import *
+from .debug        import set_debug
+from .dimensions   import dimensions
 
-from .author         import Author
-from .journal        import Journal
-from .organization   import Organization
-from .person         import Person
-from .publication    import Publication
-from .researcher     import Researcher
+from .author       import Author
+from .journal      import Journal
+from .organization import Organization
+from .person       import Person
+from .publication  import Publication
+from .researcher   import Researcher
