e3c0f0d8b00567b7c0cab5aaa9d3f5bb56e13f2f
Michael Hucka <mhucka@caltech.edu>
2019-03-05 17:53:42 -0800

    Add another example

---
+++ examples/find-sbml-pubs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    pubs-missing-dois.py
+# @brief   Example use of Sidewall to print info about pubs by Caltech people
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/sidewall
+# =============================================================================
+
+import os
+import sys
+
+# Allow this program to be executed directly from the 'examples' directory.
+try:
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(thisdir, '..'))
+except:
+    sys.path.insert(0, '..')
+
+import sidewall
+from sidewall import dimensions
+
+if len(sys.argv) > 1 and sys.argv[1] == '-d':
+    sidewall.set_debug(True)
+
+dimensions.login()
+
+print('sending query to Dimensions')
+pubs = dimensions.query('search publications in title_abstract_only for "SBML" return publications')
+print('got back {} publications'.format(len(pubs)))
+
+for p in pubs:
+    print('({}) {} - {}'.format(p.year, p.doi, p.title), )
