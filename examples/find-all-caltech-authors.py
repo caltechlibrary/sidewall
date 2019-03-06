a6e2fb68f3ba8ae422373b4cac06a6585dff7432
Michael Hucka <mhucka@caltech.edu>
2019-03-06 14:46:12 -0800

    Add another example

---
+++ examples/find-all-caltech-authors.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    print-caltech-author-affiliations.py
+# @brief   Example use of Sidewall to find publications with Caltech authors
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/sidewall
+# =============================================================================
+
+import os
+import sys
+
+# Allow this program to be executed directly from the 'examples' directory.
+try:
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(thisdir, '..'))
+except:
+    sys.path.insert(0, '..')
+
+import sidewall
+from sidewall import dimensions, Researcher
+
+if len(sys.argv) > 1 and sys.argv[1] == '-d':
+    sidewall.set_debug(True)
+
+print('Logging in to Dimensions')
+dimensions.login()
+
+print('Sending query to Dimensions')
+results = dimensions.query('search publications where research_orgs.id = "grid.20861.3d" and year<2010 return publications', limit_results = 100)
+
+print('Found {} publications from Dimensions'.format(len(results)))
+
+techers = []
+for publication in results:
+    # A publication may have coauthors who are not from Caltech.
+    # The author affiliations in the publication may also not reflect all the
+    # affiliations for a given author.  This expands author info to get full
+    # affiliations and then uses that.
+    for author in publication.authors:
+        researcher = Researcher(author)
+        for org in researcher.affiliations:
+            if (org.id == "grid.20861.3d"
+                or org.name == "California Institute of Technology"):
+                print(researcher)
+                techers.append(researcher)
+
+print('Found {} authors with Caltech affiliations'.format(len(techers)))
+for person in techers:
+    print('{} {} ({})'.format(person.first_name, person.last_name,
+                              person.orcid if person.orcid else 'no orcid'))
