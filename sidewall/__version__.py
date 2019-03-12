8f37d2fa6069d5d65f77075566c2ce3cd5ffc741
Michael Hucka <mhucka@caltech.edu>
2019-03-12 13:54:01 -0700

    Version bump for latest changes

--- sidewall/__version__.py
+++ sidewall/__version__.py
@@ -6,7 +6,7 @@
 # @website https://github.com/caltechlibrary/sidewall
 # =============================================================================
 
-__version__     = '0.6.0'
+__version__     = '0.7.0'
 __title__       = 'Sidewall'
 __name__        = 'Sidewall'
 __description__ = '''Simple Dimensions Wrapper Client Library.'''
