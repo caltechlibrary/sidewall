b60efd73f9ca0238694d9eef2dfa8036de3e394f
Michael Hucka <mhucka@caltech.edu>
2019-03-13 20:39:43 -0700

    Fix bugs and add 'role' field

--- sidewall/researcher.py
+++ sidewall/researcher.py
@@ -23,24 +23,26 @@ from .person import Person
 
 
 class Researcher(Person):
-    _new_attributes = ['affiliations']
+    _new_attributes = ['affiliations', 'role']
     _attributes = _new_attributes + Person._attributes
 
 
-    def __init__(self, data, creator = None):
+    def __init__(self, data, creator = None, dimensions_obj = None):
         if isinstance(data, Author):
             # We're given an author object, probably obtained from a pub search,
             # and we want to fill it out to create a Researcher object.
             if __debug__: log('converting Author {} to Researcher', id(data))
-            super().__init__(data._orig_data, creator = creator)
+            super().__init__(data._orig_data, creator, dimensions_obj)
         else:
             # This is a standard initialization, not a case of upconverting.
-            super().__init__(data)
+            super().__init__(data, creator, dimensions_obj)
 
 
     def _expand_attributes(self, data):
         # Be careful not to invoke "self.x" b/c it causes infinite recursion.
         if __debug__: log('expanding attributes on {} using {}', id(self), data)
+        # When this comes from a grant, there may be a 'role' field.
+        set_objattr(self, 'role', data.get('role', ''), overwrite = True)
         self._set_affiliations(data)
 
 
@@ -50,24 +52,20 @@ class Researcher(Person):
         self._set_affiliations(data)
 
 
-    def _set_affiliations(self, data):
-        if 'research_orgs' not in data or len(data['research_orgs']) == 0:
+    def _set_affiliations(self, data, field_name = 'research_orgs'):
+        if field_name not in data or len(data[field_name]) == 0:
             return
-        try:
-            affiliations = objattr(self, 'affiliations')
-        except:
-            affiliations = []
+        affiliations = objattr(self, 'affiliations', [])
         dimensions = objattr(self, '_dimensions')
         if dimensions:
-            # The research_orgs list only has grid id's.
-            for org_id in data['research_orgs']:
-                affiliations.append(dimensions.factory(Organization, {'id': org_id}, self))
+            new_object = lambda data: dimensions.factory(Organization, data, self)
         else:
-            for org_id in data['research_orgs']:
-                affiliations.append(Organization({'id': org_id}, self))
+            new_object = lambda data: Organization(data, self)
+        for org_data in data[field_name]:
+            for existing_org in affiliations:
+                if org_data.id == existing_org.id:
+                    existing_org._set_attributes(org_data, overwrite = False)
+                    break
+            else: # This 'else' is for the inner 'for' loop, not the 'if' stmt.
+                affiliations.append(new_object(org_data))
         set_objattr(self, 'affiliations', affiliations, overwrite = True)
-
-
-    def __repr__(self):
-        return "<Researcher {}: '{} {}'>".format(self.id, self.first_name,
-                                                 self.last_name)
