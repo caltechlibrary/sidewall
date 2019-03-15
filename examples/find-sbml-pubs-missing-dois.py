7575d49880c8e2f2d2ce53596d84b7c6183b89c7
Michael Hucka <mhucka@caltech.edu>
2019-03-15 14:24:26 -0700

    Print more information and improve overall code

--- examples/find-sbml-pubs-missing-dois.py
+++ examples/find-sbml-pubs-missing-dois.py
@@ -7,29 +7,44 @@
 # @website https://github.com/caltechlibrary/sidewall
 # =============================================================================
 
+# Path configuration
+# .............................................................................
+# The following several lines are to allow this program to be executed directly
+# from the 'examples' directory without having installed Sidewall.
+
 import os
 import sys
-
-# Allow this program to be executed directly from the 'examples' directory.
 try:
     thisdir = os.path.dirname(os.path.abspath(__file__))
     sys.path.insert(0, os.path.join(thisdir, '..'))
 except:
     sys.path.insert(0, '..')
 
+# Main program
+# .............................................................................
+# The rest of this file is the actual code for the example.
+
 import sidewall
 from sidewall import dimensions
 
+# Turn on debugging mode if executed with command-line option "-d".
 if len(sys.argv) > 1 and sys.argv[1] == '-d':
     sidewall.set_debug(True)
 
+print('Logging in to Dimensions')
 dimensions.login()
 
-print('sending query to Dimensions')
+print('Sending query to Dimensions')
 pubs = dimensions.query('search publications in title_abstract_only for "SBML" return publications')
-print('got back {} publications'.format(len(pubs)))
 
-print('SBML publications missing DOIs:')
-for p in pubs:
-    if not p.doi:
-        print('({}) {}'.format(p.year, p.title))
+print('Got back {} publications'.format(len(pubs)))
+print('Listing publications that lack DOIs:')
+for p in filter(lambda record: not record.doi, pubs):
+    print('')
+    author = p.authors[0].last_name + (' et al.' if len(p.authors) > 1 else '')
+    title = '"' + p.title + '"'
+    print('({}) {}, {}'.format(p.year, author, title))
+    if p.type == 'article':
+        print('{} {}({}):{}'.format(p.journal.title, p.volume, p.issue, p.pages))
+    elif p.book_title:
+        print('In "{}"'.format(p.book_title))
